2e216adedf4d5e441cbd45ed729a1b6a443e551d
hahuy1708 <hahuy17082005@gmail.com>
2026-01-10 18:16:02 +0700

    feat: add Google and GitHub login views for social authentication

--- backend/devblogg_backend/devblogg_auth/views.py
+++ backend/devblogg_backend/devblogg_auth/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# devblogg_auth/views.py
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.conf import settings
 
-# Create your views here.
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://localhost:5173" 
+    client_class = OAuth2Client
+
+class GitHubLogin(SocialLoginView):
+    adapter_class = GitHubOAuth2Adapter
+    callback_url = "http://localhost:5173"
+    client_class = OAuth2Client
